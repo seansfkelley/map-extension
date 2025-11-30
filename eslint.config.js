c31c7135b5a59190ad40f58d62531a3e6ed71cdf
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-11-29 16:25:19 -0800

    Initial commit a la Claude.

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];
---
+++ src/__tests__/content.test.ts
@@ -0,0 +1,5 @@
+describe('content script', () => {
+  it('should pass a basic test', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
