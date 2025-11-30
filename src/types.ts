ab5d44958e54af70c6e05373e4ff1abfe292d178
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-11-30 09:42:47 -0800

    Add the remaining projections.

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,15 @@
 export const _projections = [
+  // https://xkcd.com/977/
+  'Van der Grinten',
   'Robinson',
-  'Gall-Peters',
-  'Goode Homolosine',
-  'Equirectangular',
-  'Waterman Butterfly',
   'Dymaxion',
+  'Winkel-Tripel',
+  'Goode Homolosine',
+  'Hobo-Dyer',
+  'Plate Carrée (Equirectangular)',
+  'Waterman Butterfly',
+  'Gall-Peters',
+  'Peirce Quincunial',
 ] as const;
 export const PROJECTIONS = _projections as readonly Projection[];
 export type Projection = (typeof _projections)[number];
